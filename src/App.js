80cc4f8b1f8254cb0e8ca240d47b6e82794bdfb8
VaxobjanovDev <VaxobjanovDev@gmail.com>
2022-03-01 11:33:48 +0500

    Initialize project using Create React Cpp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import "./App.css";
+import Clock from "./components/sidebar/asdsa";
+import Home from "./pages/Home";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Home />
+      </div>
+      {/* <Clock/> */}
+    </>
   );
-}
+};
 
 export default App;
