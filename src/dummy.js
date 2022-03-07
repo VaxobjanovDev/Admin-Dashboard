61709010d6a15ed0054dc406d51335cd68eb2423
VaxobjanovDev <VaxobjanovDev@gmail.com>
2022-03-07 21:23:46 +0500

    Chart data

---
+++ src/dummy.js
@@ -0,0 +1,46 @@
+export const userData = [
+  {
+    name: "Jan",
+    "Active User": 7000,
+  },
+  {
+    name: "Feb",
+    "Active User": 9000,
+  },
+  {
+    name: "Mar",
+    "Active User": 10000,
+  },
+  {
+    name: "Apr",
+    "Active User": 5500,
+  },
+  {
+    name: "May",
+    "Active User": 6500,
+  },
+  {
+    name: "Jun",
+    "Active User": 6000,
+  },
+  {
+    name: "Jul",
+    "Active User": 7000,
+  },
+  {
+    name: "Aug",
+    "Active User": 8500,
+  },
+  {
+    name: "Sep",
+    "Active User": 7500,
+  },
+  {
+    name: "Oct",
+    "Active User": 4500,
+  },
+  {
+    name: "Dec",
+    "Active User": 6000,
+  },
+];
